eef757b2f43b9bff27aee267d8ad17c58d2f5ece
Priyanka Agrawal <priyankaagrawal@Priyankas-MacBook-Pro.local>
2022-12-11 22:36:54 -0500

    added prompts to javascript

--- script.js
+++ script.js
@@ -2,9 +2,22 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+function writePassword(event) {
+  event.preventDefault();
   var passwordText = document.querySelector("#password");
+
+  var passLength = prompt("Length of password", "Choose between 8-20 characters");
+  if (passLength < 8 || passLength > 20) {
+    alert("Password length must be between 8 and 20 characters")
+  }
+  var passUpper = confirm("Should password include UPPERCASE? Click OK for yes and CANCEL for no");
+  var passLower = confirm("Should password include LOWERCASE? Click OK for yes and CANCEL for no");
+  var passNumber = confirm("Should password include NUMERIC VALUES? Click OK for yes and CANCEL for no");
+  var passSpecial = confirm("Should password include SPECIAL CHARACTERS? Click OK for yes and CANCEL for no");
+  //prompt("Special characters to be included", "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~");
+  
+
+  
 
   passwordText.value = password;
 
