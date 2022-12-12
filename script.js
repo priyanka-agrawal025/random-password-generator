d12d43dd92ee513669bb8aace421d5c89846dfd4
Priyanka Agrawal <priyankaagrawal@Priyankas-MacBook-Pro.local>
2022-12-12 17:14:54 -0500

    final update of javascript code is in working order

--- script.js
+++ script.js
@@ -1,58 +1,4 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword(event) {
-  event.preventDefault();
-  var passwordText = document.querySelector("#password");
-  var passLength = prompt("Length of password", "Choose between 8-20 characters");
-  while (passLength < 8 || passLength > 20) {
-    alert("Password length must be between 8 and 20 characters");
-    passLength = prompt("Length of password", "Choose between 8-20 characters");
-  }
-
-  var passUpper = confirm("Should password include UPPERCASE? Click OK for yes and CANCEL for no");
-  var passLower = confirm("Should password include LOWERCASE? Click OK for yes and CANCEL for no");
-  var passNumber = confirm("Should password include NUMERIC VALUES? Click OK for yes and CANCEL for no");
-  var passSpecial = confirm("Should password include SPECIAL CHARACTERS? Click OK for yes and CANCEL for no");
-  
-  var password = "";
-  for (i = 1; i <= passLength; i += 1){
-    if (passUpper === true) {
-      password += getRandomUpper();
-      i++;
-    } 
-    if (passLower === true) {
-      password += getRandomLower();
-      i++;
-    }
-    if (passNumber === true) {
-      password += getRandomNumber();
-      i++;
-    }
-    if (passSpecial === true) {
-      password += getRandomSymbol();
-      i++;
-    } 
-    if (!passUpper && !passLower && !passNumber && !passSpecial) {
-      password += getRandomLower();
-      i++;
-    }
-  }
-
-
-  
-
-  
-
-  
-  //prompt("Special characters to be included", "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"); 
-
-  passwordText.value = password;
-
-}
-
-
 
 function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
@@ -71,5 +17,42 @@ function getRandomSymbol() {
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
-// Add event listener to generate button
+function writePassword(event) {
+  event.preventDefault();
+  var passwordText = document.querySelector("#password");
+
+  var passLength = prompt("Length of password", "Choose between 8-20 characters");
+  while (passLength < 8 || passLength > 20) {
+    alert("Password length must be between 8 and 20 characters");
+    passLength = prompt("Length of password", "Choose between 8-20 characters");
+  }
+
+  var passUpper = confirm("Should password include UPPERCASE? Click OK for yes and CANCEL for no");
+  var passLower = confirm("Should password include LOWERCASE? Click OK for yes and CANCEL for no");
+  var passNumber = confirm("Should password include NUMERIC VALUES? Click OK for yes and CANCEL for no");
+  var passSpecial = confirm("Should password include SPECIAL CHARACTERS? Click OK for yes and CANCEL for no");
+  
+  var password = "";
+
+  for (i = 1; i <= passLength; i += 1){
+    if (passUpper === true) {
+      password += getRandomUpper();
+      i++;
+    } 
+    if (passLower === true) {
+      password += getRandomLower();
+      i++;
+    }
+    if (passNumber === true) {
+      password += getRandomNumber();
+      i++;
+    }
+    if (passSpecial === true) {
+      password += getRandomSymbol();
+      i++;
+    } 
+  }
+  passwordText.value = password;
+}
+
 generateBtn.addEventListener("click", writePassword);
