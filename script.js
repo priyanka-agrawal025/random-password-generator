5d86c9c512b618638fdf49db3652f6eefcf10c35
Priyanka Agrawal <priyankaagrawal@Priyankas-MacBook-Pro.local>
2022-12-11 23:19:32 -0500

    added while loop for prompt and randomizer functions

--- script.js
+++ script.js
@@ -5,22 +5,47 @@ var generateBtn = document.querySelector("#generate");
 function writePassword(event) {
   event.preventDefault();
   var passwordText = document.querySelector("#password");
-
   var passLength = prompt("Length of password", "Choose between 8-20 characters");
-  if (passLength < 8 || passLength > 20) {
-    alert("Password length must be between 8 and 20 characters")
+  while (passLength < 8 || passLength > 20) {
+    alert("Password length must be between 8 and 20 characters");
+    passLength = prompt("Length of password", "Choose between 8-20 characters");
   }
+
   var passUpper = confirm("Should password include UPPERCASE? Click OK for yes and CANCEL for no");
   var passLower = confirm("Should password include LOWERCASE? Click OK for yes and CANCEL for no");
   var passNumber = confirm("Should password include NUMERIC VALUES? Click OK for yes and CANCEL for no");
   var passSpecial = confirm("Should password include SPECIAL CHARACTERS? Click OK for yes and CANCEL for no");
-  //prompt("Special characters to be included", "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~");
+  
+  var password = "";
+  for (i = 1; i <= passLength; i++){
+    password += getRandomUpper();
+  }
+
   
 
   
+  //prompt("Special characters to be included", "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"); 
 
   passwordText.value = password;
 
+}
+
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+ 
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+ 
+function getRandomSymbol() {
+  const symbols = "!@#$%^&*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
 // Add event listener to generate button
